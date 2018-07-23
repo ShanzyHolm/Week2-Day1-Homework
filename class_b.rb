0bd24fe0771ce6f7fe069095891060e7094fb40f
Shannon Holmes <snholmes13@gmail.com>
2018-07-23 23:30:51 +0100

    add test team name

---
+++ class_b.rb
@@ -0,0 +1,16 @@
+class Team
+
+  def initialize(team_name, players, coach)
+
+    @team_name = team_name
+    @players = players
+    @coach = coach
+
+  end
+
+# GETTERS
+  def team_name
+    return @team_name
+  end
+
+end
---
+++ specs/class_b_spec.rb
@@ -0,0 +1,19 @@
+require("MiniTest/autorun")
+require("minitest/rg")
+require_relative("../class_b.rb")
+
+class TestSportsTeam_B < MiniTest::Test
+
+  def setup()
+
+    @team = Team.new("win-less", ["Snoopy", "Lucy", "Linus", "Woodstock"], "Charlie Brown")
+
+  end
+
+  def test_team_name
+    result = @team.team_name()
+    assert_equal("win-less", result)
+  end
+
+
+end
