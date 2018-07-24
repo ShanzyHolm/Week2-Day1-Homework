1875d046338406add7028160e7d7017505581779
Shannon Holmes <snholmes13@gmail.com>
2018-07-24 10:15:48 +0100

    add extension solution find_book_by_title

--- class_c_extension.rb
+++ class_c_extension.rb
@@ -1,19 +1,38 @@
 class Library
 
-books = []
+  attr_reader :books
 
-  def initialize(book_title, rental_details, student_name, date_due)
-
-    @book_title = book_title
-    @rental_details = {student_name => date_due}
-      @student_name = student_name
-      @date = date_due
-
+  def initialize()
+    @books = []
   end
 
+  def add_book(book)
+    @books.push(book)
+  end
 
-  def book_title()
-    return @book_title
+  def find_by_title(title)
+    for book in @books
+      if book[:title] == title
+        return book
+      end
+    end
   end
 
 end
+
+
+# books = []
+#
+#   def initialize(book_title, rental_details, student_name, date_due)
+#
+#     @book_title = book_title
+#     @rental_details = {student_name => date_due}
+#       @student_name = student_name
+#       @date = date_due
+#
+#   end
+#
+#
+#   def book_title()
+#     return @book_title
+#   end
--- specs/class_c_extension_spec.rb
+++ specs/class_c_extension_spec.rb
@@ -7,20 +7,54 @@ class TestLibrary < MiniTest::Test
 
   def setup()
 
-    @book_1 = Library.new("New York Trilogy", {},"Mickey", "04/12/2018")
-    # @book_2 = Book.new("Hop on Pop", "Minnie", "22/01/2018")
-    # @book_3 = Book.new("Introduction to Ruby", "Donald", "15/12/2018")
-
-    # @library = Library.new([@book_1])
-      # , @book_2, @book_3])
+     @book =
+     {
+        title:  "The_New_York_Trilogy",
+        rental_details: {
+          student_name: "Mickey",
+          date: "01/12/2018"
+        }
+     }
+     @library = Library.new()
+     @library.add_book(@book)
 
   end
 
-  def test_get_books()
-    result = @library.book_title
-    assert_equal(["New York Trilogy"], result)
+  def test_add_books()
+    book2 =
+    {
+      title: "Hop_on_Pop",
+      rental_details: {
+        student_name: "Minnie",
+        date: "22/01/2018"
+    }
+  }
+
+    @library.add_book(book2)
+    result = @library.books
+    assert_equal(2, result.size)
+
   end
-  # "Hop on Pop" "Introduction to Ruby"
 
+  def test_get_book_by_title()
+    result = @library.find_by_title("The_New_York_Trilogy")
+    assert_equal(@book, result)
+  end
 
+#
+#     # @book_2 = Book.new()
+#     # @book_3 = Book.new("Introduction to Ruby", "Donald", "15/12/2018")
+#
+#     # @library = Library.new([@book_1])
+#       # , @book_2, @book_3])
+#
+#   end
+#
+#   def test_get_books()
+#     result = @library.book_title
+#     assert_equal(["New York Trilogy"], result)
+#   end
+#   # "Hop on Pop" "Introduction to Ruby"
+#
+#
 end
