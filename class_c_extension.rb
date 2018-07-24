8038e88aeb9e341088db9d9a4859deb9a7e6cc7f
Shannon Holmes <snholmes13@gmail.com>
2018-07-24 09:14:22 +0100

    add extension attempt

--- class_c_extension.rb
+++ class_c_extension.rb
@@ -1,9 +1,19 @@
 class Library
 
+books = []
+
+  def initialize(book_title, rental_details, student_name, date_due)
+
+    @book_title = book_title
+    @rental_details = {student_name => date_due}
+      @student_name = student_name
+      @date = date_due
+
+  end
 
 
-def initialize({book, rental_details{student_name, date})
-
-
+  def book_title()
+    return @book_title
+  end
 
 end
--- specs/class_c_extension_spec.rb
+++ specs/class_c_extension_spec.rb
@@ -3,10 +3,24 @@ require("minitest/rg")
 require_relative("../class_c_extension.rb")
 
 
-class TestLIbrary < MiniTest::Test
+class TestLibrary < MiniTest::Test
 
-def setup()
+  def setup()
 
-@library = Library.new
+    @book_1 = Library.new("New York Trilogy", {},"Mickey", "04/12/2018")
+    # @book_2 = Book.new("Hop on Pop", "Minnie", "22/01/2018")
+    # @book_3 = Book.new("Introduction to Ruby", "Donald", "15/12/2018")
+
+    # @library = Library.new([@book_1])
+      # , @book_2, @book_3])
+
+  end
+
+  def test_get_books()
+    result = @library.book_title
+    assert_equal(["New York Trilogy"], result)
+  end
+  # "Hop on Pop" "Introduction to Ruby"
+
 
 end
